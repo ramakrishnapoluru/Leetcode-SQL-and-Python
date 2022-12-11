cd66cf392d0ae9db28c9fe847e3b8eb1e7ac9863
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-10 20:38:18 -0800

    Time: 782 ms (51.17%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,11 +1,8 @@
 /* Write your T-SQL query statement below */
 
--- SELECT ISNULL(
---     (SELECT salary 
--- FROM Employee
--- ORDER BY salary DESC OFFSET 1 ROWS FETCH NEXT 1 ROWS ONLY)
---     ,NULL) AS SecondHighestSalary
 
 SELECT MAX(salary) AS SecondHighestSalary
 FROM Employee WHERE salary<(SELECT MAX(salary) AS SecondHighestSalary
 FROM Employee )
+
+
