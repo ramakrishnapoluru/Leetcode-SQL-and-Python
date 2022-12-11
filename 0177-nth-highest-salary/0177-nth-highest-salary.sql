c0cd84d8497a1eca25c87823411a11261392ce31
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-10 20:21:29 -0800

    Time: 705 ms (87.40%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -2,14 +2,9 @@ CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
 BEGIN
     RETURN (
         /* Write your T-SQL query statement below. */
-   SELECT Salary FROM 
-        (
-            SELECT
-                row_number() OVER (ORDER BY salary DESC) AS row_num,            
-                emp.salary
-            FROM (SELECT DISTINCT Salary FROM Employee) emp
-        ) result
-        WHERE row_num = @N
+  select distinct salary from Employee
+        order by salary desc
+        offset (@N-1) rows fetch next 1 rows only
         
     );
 END
