0518f74daf8e900a835222718db0fe1894bbe14a
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-11 13:22:14 -0800

    Time: 653 ms (56.90%), Space: 0B (100.00%) - LeetHub

--- 0612-shortest-distance-in-a-plane/0612-shortest-distance-in-a-plane.sql
+++ 0612-shortest-distance-in-a-plane/0612-shortest-distance-in-a-plane.sql
@@ -1,12 +1,9 @@
 /* Write your T-SQL query statement below */
 
 
--- SELECT TOP 1 ROUND(SQRT(((P2.X - P1.X) * (P2.X - P1.X) + (P2.Y - P1.Y) * (P2.Y - P1.Y))), 2) AS shortest
--- FROM Point2D P1
--- 	,Point2D P2
--- WHERE ROUND(SQRT(((P2.X - P1.X) * (P2.X - P1.X) + (P2.Y - P1.Y) * (P2.Y - P1.Y))), 2)<>0
--- ORDER BY 1
+SELECT TOP 1 ROUND(SQRT(((P2.X - P1.X) * (P2.X - P1.X) + (P2.Y - P1.Y) * (P2.Y - P1.Y))), 2) AS shortest
+FROM Point2D P1
+	,Point2D P2
+WHERE ROUND(SQRT(((P2.X - P1.X) * (P2.X - P1.X) + (P2.Y - P1.Y) * (P2.Y - P1.Y))), 2)<>0
+ORDER BY 1
 
-select ROUND(SQRT(min((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y))),2) as shortest
-from Point2D p1,Point2D p2
-where p1.x <> p2.x or p1.y <> p2.y;
