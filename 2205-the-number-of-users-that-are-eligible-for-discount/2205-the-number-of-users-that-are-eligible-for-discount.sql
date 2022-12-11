385737b734b7e645d5cc5b0b170153dc27632464
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-10 20:52:16 -0800

    Time: 2487 ms (74.07%), Space: 0B (100.00%) - LeetHub

---
+++ 2205-the-number-of-users-that-are-eligible-for-discount/2205-the-number-of-users-that-are-eligible-for-discount.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION getUserIDs(@startDate DATE, @endDate DATE, @minAmount INT) RETURNS INT AS
+BEGIN
+    RETURN (
+        /* Write your T-SQL query statement below. */
+        
+        SELECT COUNT(DISTINCT user_id) AS user_cnt  FROM Purchases 
+        WHERE amount>=@minAmount AND time_stamp BETWEEN @startDate AND @endDate
+    );
+END
