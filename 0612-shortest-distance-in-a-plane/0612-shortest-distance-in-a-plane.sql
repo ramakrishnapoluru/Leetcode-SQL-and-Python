862657c21a01f6455164cc3b00bf391214fd7403
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-11 13:20:34 -0800

    Time: 710 ms (44.83%), Space: 0B (100.00%) - LeetHub

---
+++ 0612-shortest-distance-in-a-plane/0612-shortest-distance-in-a-plane.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+
+
+SELECT TOP 1 ROUND(SQRT(((P2.X - P1.X) * (P2.X - P1.X) + (P2.Y - P1.Y) * (P2.Y - P1.Y))), 2) AS shortest
+FROM Point2D P1
+	,Point2D P2
+WHERE ROUND(SQRT(((P2.X - P1.X) * (P2.X - P1.X) + (P2.Y - P1.Y) * (P2.Y - P1.Y))), 2)<>0
+ORDER BY 1
