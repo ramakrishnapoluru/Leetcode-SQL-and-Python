0991b338531fbf7559f9e7ebc2744a9334ce6c1a
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-10 20:52:32 -0800

    Time: 2766 ms (55.56%), Space: 0B (100.00%) - LeetHub

--- 2205-the-number-of-users-that-are-eligible-for-discount/2205-the-number-of-users-that-are-eligible-for-discount.sql
+++ 2205-the-number-of-users-that-are-eligible-for-discount/2205-the-number-of-users-that-are-eligible-for-discount.sql
@@ -3,7 +3,10 @@ BEGIN
     RETURN (
         /* Write your T-SQL query statement below. */
         
-        SELECT COUNT(DISTINCT user_id) AS user_cnt  FROM Purchases 
-        WHERE amount>=@minAmount AND time_stamp BETWEEN @startDate AND @endDate
+SELECT COUNT(DISTINCT user_id) AS user_cnt
+FROM Purchases
+WHERE amount >= @minAmount
+	AND time_stamp BETWEEN @startDate
+		AND @endDate
     );
 END
