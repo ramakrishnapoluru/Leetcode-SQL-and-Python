e98ad2380a791bcfee60b671007a39bfcb0dba9d
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-10 21:38:47 -0800

    Time: 1071 ms (61.68%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,13 +1,6 @@
 /* Write your T-SQL query statement below */
 
 
--- SELECT num FROM 
--- (SELECT num, DENSE_RANK() OVER(ORDER BY id) AS DENSERANK FROM 
--- Logs) AS A 
--- GROUP BY DENSERANK
--- HAVING COUNT(num)>=3
-
-
 SELECT DISTINCT num AS ConsecutiveNums FROM (
 SELECT id, num, 
 				ROW_NUMBER() OVER (ORDER BY id) 
