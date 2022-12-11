92a740837b6745c4627743b8828a4d4e10129a8c
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-10 23:21:53 -0800

    Time: 852 ms (47.67%), Space: 0B (100.00%) - LeetHub

---
+++ 0578-get-highest-answer-rate-question/0578-get-highest-answer-rate-question.sql
@@ -0,0 +1,17 @@
+/* Write your T-SQL query statement below */
+
+SELECT question_id survey_log FROM 
+(
+SELECT TOP 1
+question_id,
+    ROUND( (  SUM(CASE WHEN action='answer' THEN 1 ELSE 0 END)*1.00/
+IIF(
+SUM(CASE WHEN action='show' THEN 1 ELSE 0 END)=0,1, 
+SUM(CASE WHEN action='show' THEN 1 ELSE 0 END)
+) *1.00 ),2)
+AS answer_Cnt
+FROM SurveyLog
+GROUP BY question_id
+ORDER BY answer_Cnt DESC,question_id asc
+) AS A
+
