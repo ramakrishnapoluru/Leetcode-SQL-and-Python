cca29009e3376029a63beba0960e3a2040056e7d
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-11 21:23:19 -0800

    Time: 2489 ms (93.37%), Space: 0B (100.00%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,18 @@
+/* Write your T-SQL query statement below */
+
+
+
+SELECT 
+ROUND(  ( SUM(DAYSBETWEEN)*1.00/(SELECT COUNT(DISTINCT player_id) FROM Activity) ),2)
+AS fraction FROM
+(
+SELECT 
+CASE WHEN 
+DATEDIFF(DAY,LAG(event_date) OVER (PARTITION BY player_id ORDER  BY player_id,event_date),event_date)=1 THEN 1
+ELSE 0 END 
+AS DAYSBETWEEN,
+ROW_NUMBER() OVER(PARTITION BY player_id ORDER  BY player_id) AS RNK
+FROM Activity
+
+) AS A
+WHERE RNK=2
