9e7f584c8690fde47cb1a6636fcabe03f10bb0cb
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-17 14:19:35 -0800

    Time: 3507 ms (100.00%), Space: 0B (100.00%) - LeetHub

---
+++ 1126-active-businesses/1126-active-businesses.sql
@@ -0,0 +1,13 @@
+/* Write your T-SQL query statement below */
+
+
+
+SELECT business_id FROM Events E JOIN  (
+SELECT event_type,
+AVG(occurences*1.00)
+average_activity
+FROM Events
+GROUP BY event_type) AS AVGS ON AVGS.event_type=E.event_type
+AND occurences>average_activity
+GROUP BY business_id
+HAVING COUNT(1)>1
