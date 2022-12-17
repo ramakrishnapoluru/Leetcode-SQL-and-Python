4b6fd315dea149bcf191dc79afdb8025e0e9fceb
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-17 13:15:42 -0800

    Time: 3035 ms (91.18%), Space: 0B (100.00%) - LeetHub

--- 1098-unpopular-books/1098-unpopular-books.sql
+++ 1098-unpopular-books/1098-unpopular-books.sql
@@ -1,16 +1,6 @@
 /* Write your T-SQL query statement below */
 
 
--- SELECT B.book_id,B.name FROM Books B 
--- LEFT JOIN Orders O ON B.book_id=O.book_id
--- WHERE 
--- ISNULL(dispatch_date,'2019-04-23') BETWEEN DATEADD(YEAR,-1,'2019-06-23') AND '2019-06-23'
--- -- AND
--- -- ISNULL(available_from,'1900-01-01')<'2019-05-23'
--- GROUP BY B.book_id,B.name
--- HAVING ISNULL(SUM(quantity),0)<10 
-
-
 SELECT B.book_id,name
 FROM Books B
 LEFT JOIN (
