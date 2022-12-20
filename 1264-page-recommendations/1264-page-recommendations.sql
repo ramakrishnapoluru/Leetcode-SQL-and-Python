70f57f6e97f55b3addf1ab245bca2e159b2ee433
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-19 18:04:12 -0800

    Time: 410 ms (100.00%), Space: 0B (100.00%) - LeetHub

---
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -0,0 +1,16 @@
+/* Write your T-SQL query statement below */
+
+SELECT DISTINCT page_id AS recommended_page
+FROM Likes
+WHERE user_id IN (
+    
+    SELECT user2_id FROM Friendship WHERE user1_id = 1
+	UNION
+	SELECT user1_id FROM Friendship WHERE user2_id = 1
+    
+    )  
+AND page_id NOT IN (
+		SELECT page_id
+		FROM Likes
+		WHERE user_id = 1
+		)
