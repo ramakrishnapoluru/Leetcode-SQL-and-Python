b074aef24d637085033ef163a0f59f6c44565e1f
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-28 16:58:33 -0800

    Time: 583 ms (97.78%), Space: 0B (100.00%) - LeetHub

--- 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,20 +1,29 @@
 /* Write your T-SQL query statement below */
 
 
-(SELECT
-C.customer_id,C.customer_name
-FROM Customers C JOIN Orders O
-ON C.customer_id=O.customer_id
-WHERE O.product_name IN ('A')
-INTERSECT
-SELECT
-C.customer_id,C.customer_name
-FROM Customers C JOIN Orders O
-ON C.customer_id=O.customer_id
-WHERE O.product_name IN ('B'))
-EXCEPT
-SELECT
-C.customer_id,C.customer_name
-FROM Customers C JOIN Orders O
-ON C.customer_id=O.customer_id
-WHERE O.product_name IN ('C')
+-- (SELECT
+-- C.customer_id,C.customer_name
+-- FROM Customers C JOIN Orders O
+-- ON C.customer_id=O.customer_id
+-- WHERE O.product_name IN ('A')
+-- INTERSECT
+-- SELECT
+-- C.customer_id,C.customer_name
+-- FROM Customers C JOIN Orders O
+-- ON C.customer_id=O.customer_id
+-- WHERE O.product_name IN ('B'))
+-- EXCEPT
+-- SELECT
+-- C.customer_id,C.customer_name
+-- FROM Customers C JOIN Orders O
+-- ON C.customer_id=O.customer_id
+-- WHERE O.product_name IN ('C')
+
+select cs.customer_id, cs.customer_name from Orders ord 
+join Customers cs
+on ord.customer_id = cs.customer_id
+group by cs.customer_id, cs.customer_name
+having sum(case when product_name='A' then 1 else 0 end)>0 and
+sum(case when product_name='B' then 1 else 0 end)>0 and
+sum(case when product_name='C' then 1 else 0 end)=0
+order by cs.customer_id
