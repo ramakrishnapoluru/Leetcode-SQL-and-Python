8fad8858da591c4afcfa5802f0e541fa75f5e8bc
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2022-12-28 16:41:49 -0800

    Time: 1711 ms (55.95%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -3,7 +3,7 @@
 
 SELECT 
 stock_name,
-SUM(CASE WHEN operation='Sell' THEN price ELSE 0 END)
--SUM(CASE WHEN operation='Buy' THEN price ELSE 0 END) AS capital_gain_loss
+SUM(CASE WHEN operation='Sell' THEN price ELSE -1*price END)
+ capital_gain_loss
 FROM Stocks
 GROUP BY stock_name
