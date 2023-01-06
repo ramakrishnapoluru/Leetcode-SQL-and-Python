f228052347cbd5705b3e13ee4dca56f64ed5b119
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-01-06 13:45:07 -0800

    Time: 1476 ms (61.67%), Space: 0B (100.00%) - LeetHub

--- 1747-leetflex-banned-accounts/1747-leetflex-banned-accounts.sql
+++ 1747-leetflex-banned-accounts/1747-leetflex-banned-accounts.sql
@@ -2,8 +2,11 @@
 
 
 
-SELECT DISTINCT L1.account_id FROM
-LogInfo L1 JOIN LogInfo L2 
-ON L1.account_id=L2.account_id
- WHERE L1.ip_address<>L2.ip_address AND
- (L2.login BETWEEN L1.login AND L1.logout)
+SELECT DISTINCT L1.account_id
+FROM LogInfo L1
+JOIN LogInfo L2 ON L1.account_id = L2.account_id
+WHERE L1.ip_address <> L2.ip_address
+	AND (
+		L2.LOGIN BETWEEN L1.LOGIN
+			AND L1.logout
+		)
