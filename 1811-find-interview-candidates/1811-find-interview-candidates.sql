7fb056ccfcf7a0ebee54167a3321dd25fb203821
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-01-12 12:49:14 -0800

    Time: 2763 ms (42.31%), Space: 0B (100.00%) - LeetHub

--- 1811-find-interview-candidates/1811-find-interview-candidates.sql
+++ 1811-find-interview-candidates/1811-find-interview-candidates.sql
@@ -1,47 +1,32 @@
-/* Write your T-SQL query statement below */
-
-
-
--- WITH CTE_RES AS (
--- SELECT 
--- MEDALS_WINNERS,
--- contest_id-LAG(contest_id,1,contest_id-1)
--- OVER(PARTITION BY MEDALS_WINNERS ORDER BY contest_id)
--- FROM (
--- SELECT contest_id,gold_medal AS MEDALS_WINNERS
--- FROM Contests
--- UNION 
--- SELECT contest_id,silver_medal AS MEDALS
--- FROM Contests
--- UNION
--- SELECT contest_id,bronze_medal AS MEDALS
--- FROM Contests
--- ) AS A
--- )
--- SELECT MEDALS_WINNERS,
-
--- FROM CTE_RES
--- ORDER BY MEDALS_WINNERS DESC
-
 WITH CTE_RANK
 AS (
 	SELECT contest_id
-		,gold_medal as MEDALS_WINNERS,name,mail
-	FROM Contests C1 JOIN Users U1 ON U1.user_id=C1.gold_medal
+		,gold_medal AS MEDALS_WINNERS
+		,name
+		,mail
+	FROM Contests C1
+	JOIN Users U1 ON U1.user_id = C1.gold_medal
 	
 	UNION
 	
 	SELECT contest_id
-		,silver_medal as MEDALS_WINNERS,name,mail
-	FROM Contests C1 JOIN Users U1 ON U1.user_id=C1.silver_medal
+		,silver_medal AS MEDALS_WINNERS
+		,name
+		,mail
+	FROM Contests C1
+	JOIN Users U1 ON U1.user_id = C1.silver_medal
 	
 	UNION
 	
 	SELECT contest_id
-		,bronze_medal as MEDALS_WINNERS,name,mail
-	FROM Contests C1 JOIN Users U1 ON U1.user_id=C1.bronze_medal
+		,bronze_medal AS MEDALS_WINNERS
+		,name
+		,mail
+	FROM Contests C1
+	JOIN Users U1 ON U1.user_id = C1.bronze_medal
 	)
-SELECT DISTINCT c1.name,c1.mail
+SELECT DISTINCT c1.name
+	,c1.mail
 FROM CTE_RANK C1
 JOIN CTE_RANK C2 ON C1.contest_id <> C2.contest_id
 JOIN CTE_RANK C3 ON C1.contest_id <> C3.contest_id
@@ -51,8 +36,13 @@ JOIN CTE_RANK C3 ON C1.contest_id <> C3.contest_id
 	AND C1.MEDALS_WINNERS = C2.MEDALS_WINNERS
 	AND C2.MEDALS_WINNERS = C3.MEDALS_WINNERS
 
-UNION 
+UNION
 
-	SELECT name,mail
-	FROM Contests C1 JOIN Users U1 ON U1.user_id=C1.gold_medal
-    GROUP BY gold_medal,name,mail HAVING COUNT(DISTINCT contest_id)>=3
+SELECT name
+	,mail
+FROM Contests C1
+JOIN Users U1 ON U1.user_id = C1.gold_medal
+GROUP BY gold_medal
+	,name
+	,mail
+HAVING COUNT(DISTINCT contest_id) >= 3
