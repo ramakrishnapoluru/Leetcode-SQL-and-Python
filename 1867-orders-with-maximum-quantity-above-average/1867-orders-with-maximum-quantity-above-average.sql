27ea6441d2e6bcde49f79d530ecf7589080c7959
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-01-16 11:30:43 -0800

    Time: 2068 ms (22.45%), Space: 0B (100.00%) - LeetHub

--- 1867-orders-with-maximum-quantity-above-average/1867-orders-with-maximum-quantity-above-average.sql
+++ 1867-orders-with-maximum-quantity-above-average/1867-orders-with-maximum-quantity-above-average.sql
@@ -2,18 +2,19 @@
 
 
 
-WITH CTE_OrdersDetails as ( 
-SELECT
-DISTINCT
-order_id,
-AVG(quantity*1.00) OVER(PARTITION BY order_id) AS AVGQUANTITY,
-MAX(quantity) OVER(PARTITION BY order_id) AS MAXQUANTITY
-FROM OrdersDetails
-)
-SELECT  
-CT1.order_id
-FROM CTE_OrdersDetails  CT1
-CROSS JOIN  CTE_OrdersDetails  CT2
-WHERE CT1.MAXQUANTITY>CT2.AVGQUANTITY
+WITH CTE_OrdersDetails
+AS (
+	SELECT DISTINCT order_id
+		,AVG(quantity * 1.00) OVER (PARTITION BY order_id) AS AVGQUANTITY
+		,MAX(quantity) OVER (PARTITION BY order_id) AS MAXQUANTITY
+	FROM OrdersDetails
+	)
+SELECT CT1.order_id
+FROM CTE_OrdersDetails CT1
+CROSS JOIN CTE_OrdersDetails CT2
+WHERE CT1.MAXQUANTITY > CT2.AVGQUANTITY
 GROUP BY CT1.order_id
-HAVING COUNT(1)=(SELECT COUNT(DISTINCT order_id) FROM OrdersDetails)
+HAVING COUNT(1) = (
+		SELECT COUNT(DISTINCT order_id)
+		FROM OrdersDetails
+		)
