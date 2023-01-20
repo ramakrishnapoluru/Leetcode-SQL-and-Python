05b7a3b5949336d538686f75f30c1efd69016376
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-01-20 14:00:42 -0800

    Time: 46 ms (43.70%), Space: 13.9 MB (84.21%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,8 +1,6 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         result=''
-        # if len(strs)==1:
-        #     return strs[0]
         strs.sort(key=len)
         minlength=strs[0]
         for i in range(len(minlength)):
@@ -10,10 +8,8 @@ class Solution:
             for j in range(len(strs)):
                 if strs[0][0:i+1] in strs[j][0:i+1]:
                     counter=counter+1
-                    if counter==len(strs):
-                        result=strs[0][0:i+1]
+                else: 
+                    break
+                if counter==len(strs):
+                    result=strs[0][0:i+1]
         return result
-                
-            
-        
-            
