a12dfca6085b73941f1656e8f5633cf80fa07d38
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-01-20 02:19:09 -0800

    Time: 691 ms (87.88%), Space: 0B (100.00%) - LeetHub

--- 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
+++ 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
@@ -1,13 +1,15 @@
 /* Write your T-SQL query statement below */
 
+SELECT order_id,customer_id,order_type 
+FROM Orders
 
-SELECT * FROM Orders  
 EXCEPT
-SELECT *
+
+SELECT order_id,customer_id,order_type 
 FROM Orders
 WHERE customer_id IN (
 		SELECT customer_id
 		FROM Orders
 		WHERE order_type = 0
 		)
-AND order_type = 1
+	AND order_type = 1
