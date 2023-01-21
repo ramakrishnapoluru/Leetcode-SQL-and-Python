f58fa362ca8497aabae173857f276e15bfaabaad
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-01-21 00:32:32 -0800

    Time: 1447 ms (12.50%), Space: 0B (100.00%) - LeetHub

---
+++ 2175-the-change-in-global-rankings/2175-the-change-in-global-rankings.sql
@@ -0,0 +1,27 @@
+/* Write your T-SQL query statement below */
+
+
+WITH CTE_POINT
+AS (
+	SELECT T.team_id
+		,name
+		,ROW_NUMBER() OVER (
+			ORDER BY points DESC,name
+			) AS RNK
+	FROM TeamPoints T
+	)
+	,CTE_PC
+AS (
+	SELECT T.team_id
+		,name
+		,ROW_NUMBER() OVER (
+			ORDER BY (points + points_change) DESC,t.name
+			) PCRK
+	FROM TeamPoints T
+	INNER JOIN PointsChange P ON T.team_id = P.team_id
+	)
+SELECT PC1.team_id
+	,PC1.name
+	,RNK - PCRK AS rank_diff
+FROM CTE_POINT PC1
+INNER JOIN CTE_PC PC2 ON PC1.team_id = PC2.team_id
