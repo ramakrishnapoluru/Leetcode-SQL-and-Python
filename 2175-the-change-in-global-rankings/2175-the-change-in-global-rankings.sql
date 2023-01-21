6a40a4508fc1c652f54065f8bdca24763db179b1
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-01-21 00:33:57 -0800

    Time: 715 ms (93.75%), Space: 0B (100.00%) - LeetHub

--- 2175-the-change-in-global-rankings/2175-the-change-in-global-rankings.sql
+++ 2175-the-change-in-global-rankings/2175-the-change-in-global-rankings.sql
@@ -1,33 +1,28 @@
 /* Write your T-SQL query statement below */
 
 
--- WITH CTE_POINT
--- AS (
--- 	SELECT T.team_id
--- 		,name
--- 		,ROW_NUMBER() OVER (
--- 			ORDER BY points DESC,name
--- 			) AS RNK
--- 	FROM TeamPoints T
--- 	)
--- 	,CTE_PC
--- AS (
--- 	SELECT T.team_id
--- 		,name
--- 		,ROW_NUMBER() OVER (
--- 			ORDER BY (points + points_change) DESC,t.name
--- 			) PCRK
--- 	FROM TeamPoints T
--- 	INNER JOIN PointsChange P ON T.team_id = P.team_id
--- 	)
--- SELECT PC1.team_id
--- 	,PC1.name
--- 	,RNK - PCRK AS rank_diff
--- FROM CTE_POINT PC1
--- INNER JOIN CTE_PC PC2 ON PC1.team_id = PC2.team_id
+WITH CTE_POINT
+AS (
+	SELECT T.team_id
+		,name
+		,ROW_NUMBER() OVER (
+			ORDER BY points DESC,name
+			) AS RNK
+	FROM TeamPoints T
+	)
+	,CTE_PC
+AS (
+	SELECT T.team_id
+		,name
+		,ROW_NUMBER() OVER (
+			ORDER BY (points + points_change) DESC,t.name
+			) PCRK
+	FROM TeamPoints T
+	INNER JOIN PointsChange P ON T.team_id = P.team_id
+	)
+SELECT PC1.team_id
+	,PC1.name
+	,RNK - PCRK AS rank_diff
+FROM CTE_POINT PC1
+INNER JOIN CTE_PC PC2 ON PC1.team_id = PC2.team_id
 
-SELECT T.team_id
-     , T.name
-     , ROW_NUMBER()OVER(ORDER BY T.points DESC, T.name ASC) - 
-        ROW_NUMBER()OVER(ORDER BY T.points + P.points_change DESC, T.name ASC) AS rank_diff
-FROM TeamPoints T LEFT JOIN PointsChange P ON T.team_id = P.team_id;
