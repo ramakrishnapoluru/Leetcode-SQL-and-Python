ec7f941d4cbf86093f81528df9ba0bef11ac19ab
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-01-26 13:15:24 -0800

    Time: 975 ms (84.68%), Space: 0B (100.00%) - LeetHub

--- 0569-median-employee-salary/0569-median-employee-salary.sql
+++ 0569-median-employee-salary/0569-median-employee-salary.sql
@@ -1,11 +1,18 @@
 /* Write your T-SQL query statement below */
 
-SELECT id,company,salary
-FROM 
-(
-SELECT id,company,salary,   
-ROW_NUMBER() OVER(PARTITION BY company ORDER BY salary,ID ) RANKING,
-COUNT(1) OVER(PARTITION BY company ) RANKING_COUNT
-FROM Employee
-) AS A
-WHERE RANKING BETWEEN CEILING(RANKING_COUNT*1.00/2) AND FLOOR(RANKING_COUNT*1.00/2)+1
+SELECT id
+	,company
+	,salary
+FROM (
+	SELECT id
+		,company
+		,salary
+		,ROW_NUMBER() OVER (
+			PARTITION BY company ORDER BY salary
+				,ID
+			) RANKING
+		,COUNT(1) OVER (PARTITION BY company) RANKING_COUNT
+	FROM Employee
+	) AS A
+WHERE RANKING BETWEEN CEILING(RANKING_COUNT * 1.00 / 2)
+		AND FLOOR(RANKING_COUNT * 1.00 / 2) + 1
