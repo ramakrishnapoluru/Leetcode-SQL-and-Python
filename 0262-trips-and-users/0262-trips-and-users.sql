9b3d438ef1c8c85ebb6cb05fe31a2b0e8d9df7f9
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-01-26 12:55:32 -0800

    Time: 599 ms (41.52%), Space: 0B (100.00%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -22,11 +22,17 @@
 -- WHERE banned = 'No'
 -- GROUP BY request_at
 
-select request_at as day, 
-cast(sum(case when status <> 'completed' then 1.0 else 0.0 end)/count(*) as decimal(10,2)) as 'cancellation rate' 
-from trips t join users rider on t.client_id = rider.users_id 
-join users driver on t.driver_id = driver.users_id
-where t.request_at between '2013-10-01' and '2013-10-03'
-and rider.banned = 'No'
-and driver.banned = 'No'
-group by request_at
+SELECT request_at AS day
+	,cast(sum(CASE 
+				WHEN STATUS <> 'completed'
+					THEN 1.0
+				ELSE 0.0
+				END) / count(*) AS DECIMAL(10, 2)) AS 'cancellation rate'
+FROM trips t
+JOIN users rider ON t.client_id = rider.users_id
+JOIN users driver ON t.driver_id = driver.users_id
+WHERE t.request_at BETWEEN '2013-10-01'
+		AND '2013-10-03'
+	AND rider.banned = 'No'
+	AND driver.banned = 'No'
+GROUP BY request_at
