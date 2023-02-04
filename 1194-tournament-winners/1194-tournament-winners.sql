8c4195b02023bc2051fa7b1bf762dd43e2f5e516
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-02-03 16:29:33 -0800

    Time: 2127 ms (39.19%), Space: 0B (100.00%) - LeetHub

---
+++ 1194-tournament-winners/1194-tournament-winners.sql
@@ -0,0 +1,26 @@
+/* Write your T-SQL query statement below */
+
+WITH CTE_Players_Matches AS (
+SELECT group_id
+	,player_id
+	,RANK() OVER (
+		PARTITION BY group_id
+	 ORDER BY SUM(CASE 
+					WHEN first_player = player_id
+						THEN first_score
+					WHEN second_player = player_id
+						THEN second_score
+					END) DESC,player_id
+		) AS SCORES_RANK
+FROM Players P
+JOIN Matches M ON player_id = first_player
+	OR second_player = player_id
+GROUP BY group_id
+	,player_id
+)
+SELECT group_id,player_id
+FROM CTE_Players_Matches 
+WHERE SCORES_RANK=1
+
+
+
