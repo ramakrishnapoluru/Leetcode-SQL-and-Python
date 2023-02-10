0cd5dbb20742adc7cd0c7737fc589854b3e70a9e
ramakrishnapoluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-02-09 20:59:39 -0800

    Time: 66 ms (23.62%), Space: 13.9 MB (54.86%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -5,9 +5,7 @@ class Solution:
         for i in range(len(prices)-1):
             for j in prices[i+1:]:
                 if prices[i]>=j:
-                    print(prices[i]-j)
                     discount.append(prices[i]-j)
-                    print(discount)
                     break
             else:
                 discount.append(prices[i])
