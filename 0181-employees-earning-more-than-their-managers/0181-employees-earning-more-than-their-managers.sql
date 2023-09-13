5e04bdd5342649b6b5ebf9fd67f130dab59ec1bd
Rama Krishna Poluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-09-13 01:00:06 -0700

    Time: 1139 ms (40.94%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,7 +1,10 @@
 /* Write your T-SQL query statement below */
 
 
-SELECT E1.name AS Employee
+SELECT E1.name as Employee
 FROM Employee E1
-JOIN Employee E2 ON E1.managerId = E2.id
-WHERE E1.salary > E2.salary
+CROSS JOIN Employee E2 
+	-- AND E2.managerid IS NOT NULL
+WHERE E1.managerid = E2.id
+	AND E1.salary > E2.salary
+    
