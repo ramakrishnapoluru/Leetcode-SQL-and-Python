3fb36f12cf20172cf61746d165171696f40734bb
Rama Krishna Poluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2023-09-13 10:47:37 -0700

    Time: 932 ms (44.36%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,7 +1,6 @@
 /* Write your T-SQL query statement below */
 
-
-SELECT name AS Customers
-FROM Customers C
-LEFT JOIN Orders O ON C.id = O.customerId
-WHERE O.id IS NULL
+SELECT name as Customers 
+FROM Customers C LEFT JOIN Orders O 
+ON C.id=O.customerId
+WHERE CustomerId IS NULL
