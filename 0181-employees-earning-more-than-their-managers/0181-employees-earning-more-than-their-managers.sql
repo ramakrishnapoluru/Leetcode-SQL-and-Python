d5b454db6fe7867e882ba656857f3e83c1aa21da
Rama Krishna Poluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2024-03-13 10:40:22 -0700

    Time: 1260 ms (32.91%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,9 +1,6 @@
 /* Write your T-SQL query statement below */
 
-
-    
-
-SELECT E1.name AS Employee
-FROM Employee E1
-JOIN Employee E2 ON E1.managerId = E2.id
-WHERE E1.salary > E2.salary
+SELECT 
+E1.name AS Employee
+FROM Employee E1 LEFT JOIN Employee E2 ON E1.managerId=E2.ID
+WHERE E1.salary>E2.salary
