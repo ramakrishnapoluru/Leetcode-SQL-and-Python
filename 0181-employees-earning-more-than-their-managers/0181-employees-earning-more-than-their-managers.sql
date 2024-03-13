8588ec7fac95f5cf6111d892258761c97efe4a31
Rama Krishna Poluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2024-03-13 10:41:22 -0700

    Time: 1383 ms (23.82%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -2,5 +2,5 @@
 
 SELECT 
 E1.name AS Employee
-FROM Employee E1 INNER JOIN Employee E2 ON E1.managerId=E2.ID
+FROM Employee E1 LEFT JOIN Employee E2 ON E1.managerId=E2.ID
 WHERE E1.salary>E2.salary
