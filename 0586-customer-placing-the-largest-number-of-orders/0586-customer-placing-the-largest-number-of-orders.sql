27ceb1df91338845bae5aa168a21cc1eba8c085b
Rama Krishna Poluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2024-03-13 15:45:04 -0700

    Time: 1461 ms (25.09%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,15 +1,9 @@
 /* Write your T-SQL query statement below */
 
 
-WITH CTE_Orders
-AS (
-	SELECT TOP 1 customer_number
-		,COUNT(order_number) AS CNT
-	FROM Orders
-	GROUP BY customer_number
-	ORDER BY 2 DESC
-	)
-SELECT TOP 1 customer_number
-FROM CTE_Orders
-
-
+SELECT TOP 1
+customer_number
+FROM 
+Orders
+GROUP BY customer_number
+ORDER BY COUNT(order_number) DESC
