03e38f5e0917d820cd81a2d36e40a0d280342c17
Rama Krishna Poluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2024-04-07 01:50:57 -0700

    Time: 2056 ms (68.57%), Space: 0B (100.00%) - LeetHub

---
+++ 1142-user-activity-for-the-past-30-days-ii/1142-user-activity-for-the-past-30-days-ii.sql
@@ -0,0 +1,16 @@
+/* Write your T-SQL query statement below */
+
+
+
+-- SELECT ROUND(COUNT(DISTINCT session_id) * 1.00 /
+-- IIF(COUNT(DISTINCT user_id) = 0, 1, COUNT(DISTINCT user_id)), 2) AS average_sessions_per_user
+-- FROM Activity
+-- WHERE activity_date BETWEEN DATEADD(DAY, - 30, '2019-07-27')
+-- 		AND '2019-07-27'
+-- 	AND activity_type NOT IN (
+-- 		'open_session'
+-- 		,'end_session'
+-- 		)
+
+SELECT ISNULL(ROUND(CAST(COUNT(DISTINCT session_id) AS DECIMAL(10,2))/NULLIF(COUNT(DISTINCT user_id),0),2),0) AS average_sessions_per_user FROM Activity
+WHERE activity_date>DATEADD(day,-30,'2019-07-27') AND activity_date<='2019-07-27'
