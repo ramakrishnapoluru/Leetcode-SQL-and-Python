6fbaa5c0ed82508d1fcdf2451ed6fc771347b501
Rama Krishna Poluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2024-04-20 15:50:11 -0700

    Time: 893 ms (63.18%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -2,10 +2,9 @@
 
 
 
-
-SELECT
-COALESCE(E.employee_id,S.employee_id) AS employee_id
-FROM Employees E FULL JOIN Salaries S
-ON E.employee_id=S.employee_id
-WHERE E.employee_id IS NULL OR S.employee_id IS NULL
+SELECT COALESCE(E.employee_id, S.employee_id) AS employee_id
+FROM Employees E
+FULL JOIN Salaries S ON E.employee_id = S.employee_id
+WHERE E.employee_id IS NULL
+	OR S.employee_id IS NULL
 ORDER BY 1
