fca610c591e5f5a84764859c71fc7f229f1f5321
Rama Krishna Poluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2024-04-20 16:08:25 -0700

    Time: 1641 ms (93.55%), Space: 0B (100.00%) - LeetHub

---
+++ 2072-the-winner-university/2072-the-winner-university.sql
@@ -0,0 +1,27 @@
+/* Write your T-SQL query statement below */
+
+
+
+SELECT CASE 
+		WHEN (
+				SELECT COUNT(1)
+				FROM NewYork
+				WHERE score >= 90
+				) > (
+				SELECT COUNT(1)
+				FROM California
+				WHERE score >= 90
+				)
+			THEN 'New York University'
+		WHEN (
+				SELECT COUNT(1)
+				FROM NewYork
+				WHERE score >= 90
+				) < (
+				SELECT COUNT(1)
+				FROM California
+				WHERE score >= 90
+				)
+			THEN 'California University'
+		ELSE 'No Winner'
+		END AS winner
