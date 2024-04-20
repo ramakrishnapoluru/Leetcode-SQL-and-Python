d4e5217f064c2e19dc2184f6b997f145c5e8f1e2
Rama Krishna Poluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2024-04-20 16:15:51 -0700

    Time: 802 ms (96.43%), Space: 0B (100.00%) - LeetHub

--- 2082-the-number-of-rich-customers/2082-the-number-of-rich-customers.sql
+++ 2082-the-number-of-rich-customers/2082-the-number-of-rich-customers.sql
@@ -1,12 +1,10 @@
 /* Write your T-SQL query statement below */
 
-WITH CTE_Store
-AS (
-	SELECT customer_id
-	FROM Store
-	WHERE amount > 500
-	GROUP BY customer_id
-	HAVING COUNT(1) >= 1
-	)
-SELECT COUNT(1) AS rich_count
-FROM CTE_Store
+
+SELECT
+
+COUNT(DISTINCT customer_id) AS rich_count
+
+FROM Store
+
+WHERE amount>500 
