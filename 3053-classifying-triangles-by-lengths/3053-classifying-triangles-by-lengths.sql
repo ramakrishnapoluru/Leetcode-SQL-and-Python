900712d0928d72cfb8e790d4943eba2b77263ab0
Rama Krishna Poluru <119472036+ramakrishnapoluru@users.noreply.github.com>
2024-04-20 17:38:47 -0700

    Time: 503 ms (93.42%), Space: 0B (100.00%) - LeetHub

--- 3053-classifying-triangles-by-lengths/3053-classifying-triangles-by-lengths.sql
+++ 3053-classifying-triangles-by-lengths/3053-classifying-triangles-by-lengths.sql
@@ -1,17 +1,21 @@
 SELECT CASE 
-		WHEN A + B <= C
-			OR A + C <= B
-			OR B + C <= A
-
+		WHEN 
+				A + B <= C
+				OR B + C <= A
+				OR A + C < = B
+				
 			THEN 'Not A Triangle'
-		WHEN A = B
-			AND B = C
-			AND C = A
+		WHEN (
+				A = B
+				AND B = C and C=A
+				)
 			THEN 'Equilateral'
-		WHEN A = B
-			OR B = C
-			OR C = A
+		WHEN (
+				A = B
+				OR B = C
+				OR C = A
+				)
 			THEN 'Isosceles'
-		ELSE 'Scalene'
+	ELSE 'Scalene'
 		END AS triangle_type
 FROM Triangles
